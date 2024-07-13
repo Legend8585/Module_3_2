b0c978d14b7342588eb27332b4ef5a709515422a
Legend8585 <nuridddin@gmail.com>
2024-07-13 21:37:41 +0300

    Update Module_3_2.py

--- Module_3_2.py
+++ Module_3_2.py
@@ -1,3 +1,4 @@
+##
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
     standard_domains = ['.com', '.ru', '.net']
     def is_standard_email(email):
@@ -7,21 +8,21 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
             if email.endswith(domain):
                 return True
         return False
-
+##
     if not is_standard_email(sender) or not is_standard_email(recipient):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
-
+##
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
         return
-
+##
     if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-
-# Примеры вызова функции:
+##
+## Пример выполняемого кода (тесты):
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
